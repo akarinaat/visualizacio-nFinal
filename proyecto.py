af42d9b1a67e78a8b3b16edd3f5ae9d38d4d3316
Karina Amador <akarina.at@gmail.com>
2022-11-25 23:57:48 -0600

    Inicialización del proyecto final visualización

---
+++ proyecto.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import streamlit as st
+from pandasql import sqldf
+
+pysqldf = lambda q: sqldf(q, globals())
+df = pd.read_csv('dataset_Facebook.csv', sep=';')
+clean_df = df.fillna(0)
+clean_df.columns = clean_df.columns.str.replace(' ','_')
+
+#st.write(clean_df)
+
+total_reach = sqldf('''
+ Select sum(Lifetime_Post_Total_Reach) as [Numero de personas que vieron el post]
+ from clean_df
+ ''')
+
+st.write(total_reach) 
+
+
+
+
+
+
+
+# def display():
+#     st.title("Hello world")
+
+# display()
+
+

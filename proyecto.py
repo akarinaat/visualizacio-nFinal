97220c7893a3f5517708e7adfb007f7ee8156101
Karina Amador <akarina.at@gmail.com>
2022-11-26 10:52:49 -0600

    Numero de interacciones por tipo de interacción

--- proyecto.py
+++ proyecto.py
@@ -4,22 +4,55 @@ from pandasql import sqldf
 
 df = pd.read_csv('dataset_Facebook.csv', sep=';')
 clean_df = df.fillna(0)
-clean_df.columns = clean_df.columns.str.replace(' ','_')
+clean_df.columns = clean_df.columns.str.replace(' ', '_')
 
-#st.write(clean_df)
+# st.write(clean_df)
 
 total_reach = sqldf('''
  Select sum(Lifetime_Post_Total_Reach) as [Numero de personas que vieron el post]
  from clean_df
  ''')
 
-st.write(total_reach) 
+total_interacciones = sqldf('''
+select count(Lifetime_engaged_users) as [Toatal de interacciones]
+from clean_df 
+
+ ''')
+
+tipos_interracciones = sqldf('''
+select distinct Type as [Tipos de interaccion]
+from clean_df 
+
+ ''')
+
+status = sqldf('''
+select count(Lifetime_engaged_users) as [Interacciones por estatus]
+from clean_df 
+where Type = 'Status'
+ ''')
+
+photo = sqldf('''
+select count(Lifetime_engaged_users) as [Interacciones por foto]
+from clean_df 
+where Type = 'Photo'
+ ''')
+
+link = sqldf('''
+select count(Lifetime_engaged_users) as [Interacciones por foto]
+from clean_df 
+where Type = 'Link'
+ ''')
+
+video = sqldf('''
+select count(Lifetime_engaged_users) as [Interacciones por foto]
+from clean_df 
+where Type = 'Video'
+ ''')
 
 
-
-
-
-
-
-
-
+st.write(tipos_interracciones)
+st.write(total_interacciones)
+st.write(status)
+st.write(photo)
+st.write(link)
+st.write(video)

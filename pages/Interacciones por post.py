4669c1a55ea483480ac3aaed992b4ec30654ce60
Karina Amador <akarina.at@gmail.com>
2022-11-27 17:03:48 -0600

    pie chart de tipos listo

--- pages/Interacciones por post.py
+++ pages/Interacciones por post.py
@@ -3,7 +3,7 @@ import streamlit as st
 from pandasql import sqldf
 import numpy as np
 import plotly.express as px
-import matplotlib.pyplot as plt
+
 
 df = pd.read_csv('dataset_Facebook.csv', sep=';')
 clean_df = df.fillna(0)
@@ -12,56 +12,35 @@ clean_df.columns = clean_df.columns.str.replace(' ', '_')
 
 
 # Post type 
-tipos_interracciones = sqldf('''
-select distinct Type as [Tipos de interaccion]
-from clean_df 
+# tipos_interracciones = sqldf('''
+# select distinct Type as [Tipos de interaccion]
+# from clean_df 
 
- ''')
-st.radio('Tipo de interacciones',tipos_interracciones)
+#  ''')
+
 
 
 st.title('Número de personas que hicieron click en el post (Usuarios únicos)')
 
+# Unique users interactions per post
+interacciones_tipo = sqldf(''' 
+Select Type as Tipo, count(Lifetime_engaged_users) as Interacciones
+from clean_df
+group by Type
+''')
 
-status = sqldf('''
-select count(Lifetime_engaged_users) as [Interacciones por estatus]
-from clean_df 
-where Type = 'Status'
- ''')
-
-photo = sqldf('''
-select count(Lifetime_engaged_users) as [Interacciones por foto]
-from clean_df 
-where Type = 'Photo'
- ''')
-
-link = sqldf('''
-select count(Lifetime_engaged_users) as [Interacciones por foto]
-from clean_df 
-where Type = 'Link'
- ''')
-
-video = sqldf('''
-select count(Lifetime_engaged_users) as [Interaccion de usuarios únicos por video]
-from clean_df 
-where Type = 'Video'
- ''')
+# Post type
+tipos = sqldf('''
+select distinct Type from clean_df
+''')
 
 # Pie chart
-
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = tipos_interracciones
-sizes = [15, 30, 45, 10]
-explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-st.pyplot(fig1)
+fig = px.pie(interacciones_tipo, values='Interacciones', names='Tipo')
+st.plotly_chart(fig)
 
 
+# Radio
+st.radio('Tipo de interacciones',tipos)
 
 #sacar el total de interacciones por tipo
 inter_video = sqldf('''
@@ -104,12 +83,8 @@ where Type = 'Link'
 
 
 
-st.write(tipos_interracciones)
+st.write(interacciones_tipo)
 
-st.write(status)
-st.write(photo)
-st.write(link)
-st.write(video)
 st.title('Suma total de las interacciones')
 st.write(inter_video)
 st.write(inter_photo)
